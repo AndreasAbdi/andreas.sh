cc3f2e5c4546400072e738825fd697a26460a962
Andreas.Abdi <andreasabdi@yahoo.ca>
2018-02-26 23:09:12 -0800

    Added templates and added a bit of bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,103 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Container, Row, Col, Button} from 'reactstrap';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Container
+                    fluid={true}>
+                    <Row>
+                        <Col>
+                            <header className="App-header">
+                                <img src={logo} className="App-logo" alt="logo" />
+                                <h1 className="App-title">Welcome to React</h1>
+                            </header>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <p className="App-intro">
+                            To get started, edit <code>src/App.js</code> and save to reload.
+                            </p>
+                        </Col>
+                        <Col>
+                            <Button color="primary">Primary</Button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Header />
+                            <Menu />
+                            <MenuButton />
+                            <Image />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Body />
+                            <InfoSlice />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
 }
 
+//todo
+class Header extends React.Component {
+    render() {
+        return (
+            <div>this is the body</div>
+        );
+    }
+}
+
+//todo
+class Menu extends React.Component {
+    render() {
+        return (
+            <div>this is the body</div>
+        );
+    }
+}
+
+
+class MenuButton extends React.Component {
+    render() {
+        return (
+            <div>this is the body</div>
+        );
+    }
+}
+
+//todo
+class Image extends React.Component {
+    render() {
+        return (
+            <div>this is the body</div>
+        );
+    }
+}
+
+//todo
+class Body extends React.Component {
+    render() {
+        return (
+            <div>this is the body</div>
+        );
+    }
+}
+
+//todo
+class InfoSlice extends React.Component {
+    render() {
+        return (
+            <div>this is the InfoSlice</div>
+        );
+    }
+}
 export default App;
